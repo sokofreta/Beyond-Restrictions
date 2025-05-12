8a92c53a7802ece221ae543cabc006190209afa1
Seitanidis Sokratis <seitanidissokratis@gmail.com>
2025-05-12 20:14:10 +0300

    Adding Timer at camera scene and implement basic signs of detection

--- Game_files/Global/Scripts/RoomCamera.cs
+++ Game_files/Global/Scripts/RoomCamera.cs
@@ -5,17 +5,28 @@ public partial class RoomCamera : Node3D
 {
 	OmniLight3D CameraLight;
 	AnimationPlayer CameraAnimations;
+	Timer CameraTimer;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		CameraLight = GetNode<OmniLight3D>("CameraLight");
 		CameraAnimations = GetNode<AnimationPlayer>("Animations");
+
+		// Camera light Configuration
+		CameraTimer = GetNode<Timer>("TimeToDetect");
+		CameraTimer.Timeout += OnTimeOut; // When signal Timeout is emited call funation OnTimeOut;
 		CameraLight.LightEnergy = 0;
 
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    private void OnTimeOut()
+    {
+		CameraTimer.Stop();
+        CameraLight.LightColor = new Color((float)0.545098, 0, 0, 1);
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
 	{
 	}
 
@@ -23,7 +34,8 @@ public partial class RoomCamera : Node3D
 		if (body == null){
 			return;
 		}
-
+		CameraLight.LightColor = new Color(0, 1, 0, 1);
+		CameraTimer.Start(3);
 	 	CameraAnimations.Play("Detection");
 	}
 
@@ -31,7 +43,7 @@ public partial class RoomCamera : Node3D
 		if (body == null){
 			return;
 		}
-
+		CameraTimer.Stop();
 		CameraAnimations.Play("RESET");
 	}
 }
